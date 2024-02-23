dc06b7e74473bd61a3385f0a9962eafa240577f9
Anuraag Deoda <72085003+Anuraag-Deoda@users.noreply.github.com>
2024-02-23 09:20:20 +0530

    Time: 84 ms (61.45%) | Memory: 57.4 MB (39.11%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.js
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.js
@@ -7,18 +7,22 @@
  * @return {number}
  */
 var findCheapestPrice = function(n, flights, src, dst, k) {
-    const dp = Array.from({length: k + 2}, () => Array(n).fill(Infinity));
-    dp[0][src] = 0;
+    const costs = new Array(n).fill(Infinity);
+    costs[src] = 0;
 
-    for (let stops = 1; stops <= k + 1; stops++) {
-        dp[stops][src] = 0;
-        for (let [from, to, price] of flights) {
-           
-            dp[stops][to] = Math.min(dp[stops][to], dp[stops - 1][from] + price);
+    let ans = Infinity;
+    for (let i = 0; i <= k; i++) {
+        const tempCosts = [...costs]; 
+        for (const [u, v, w] of flights) {
+            if (costs[u] === Infinity) continue;
+            if (costs[u] + w < tempCosts[v]) {
+                tempCosts[v] = costs[u] + w; 
+            }
         }
+        costs.splice(0, n, ...tempCosts); 
+        ans = Math.min(ans, costs[dst]); 
     }
 
-    const answer = dp[k + 1][dst];
-    return answer === Infinity ? -1 : answer;
+    return ans === Infinity ? -1 : ans;
 
 };
